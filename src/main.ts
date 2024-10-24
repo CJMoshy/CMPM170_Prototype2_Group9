cb10bd1225e73d64bb60a039a316d4eaacc9e404
= <cjmoshy12@gmail.com>
2024-10-24 10:30:55 -0700

    Base project. Working on CI/CD pipeline

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+
+import Phaser from "phaser";
+
+const GAME_CONFIG = {
+  type: Phaser.CANVAS,
+  parent: "phaser-game",
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    size: Phaser.Scale.FIT,
+    zoom: 0.75,
+  },
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {},
+  },
+  zoom: 1,
+  scene: [],
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Phaser.Game(GAME_CONFIG);
+});
