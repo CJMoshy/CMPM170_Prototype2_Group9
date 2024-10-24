92e56642ab85f3143a8cb67ed631944c7587a85a
= <cjmoshy12@gmail.com>
2024-10-24 15:12:31 -0700

    formatting rules update

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import "./styles/style.css";
+import './styles/style.css';
 
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const GAME_CONFIG = {
-  type: Phaser.CANVAS,
-  parent: "phaser-game",
-  scale: {
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    size: Phaser.Scale.FIT,
-    zoom: 0.75,
-  },
-  pixelArt: true,
-  physics: {
-    default: "arcade",
-    arcade: {},
-  },
-  zoom: 1,
-  scene: [],
+	type: Phaser.CANVAS,
+	parent: 'phaser-game',
+	scale: {
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		size: Phaser.Scale.FIT,
+		zoom: 0.75,
+	},
+	pixelArt: true,
+	physics: {
+		default: 'arcade',
+		arcade: {},
+	},
+	zoom: 1,
+	scene: [],
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  new Phaser.Game(GAME_CONFIG);
+document.addEventListener('DOMContentLoaded', () => {
+	new Phaser.Game(GAME_CONFIG);
 });
