a3256f5c2ff4ddf72b353b5206667ce4ae50c84b
= <cjmoshy12@gmail.com>
2024-10-27 19:55:34 -0700

    update paths

--- src/scenes/Loader.ts
+++ src/scenes/Loader.ts
@@ -11,13 +11,13 @@ export default class Loader extends Phaser.Scene {
 		this.load.audio('abyss', '../../assets/audio/abyss.wav');
 		this.load.audio('click', '../../assets/audio/click.wav');
 
-		this.load.image('player', '../../assets/blue_townie.png');
-		this.load.image('bones', '../../assets/bonepile.png');
-		this.load.image('stash', '../../assets/stash.png');
+		this.load.image('player', '../../assets/img/blue_townie.png');
+		this.load.image('bones', '../../assets/img/bonepile.png');
+		this.load.image('stash', '../../assets/img/stash.png');
 		this.load.bitmapFont(
 			'rocketSquare',
-			'../../assets/KennyRocketSquare_0.png',
-			'../../assets/KennyRocketSquare.fnt',
+			'../../assets/font/KRS/KennyRocketSquare_0.png',
+			'../../assets/font/KRS/KennyRocketSquare.fnt',
 		);
 	}
 	create() {
