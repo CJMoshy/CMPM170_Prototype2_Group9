71b4c20352456b07ed5f5e10fd3e2480434f1052
Henry <hachrist@ucsc.edu>
2024-10-31 17:19:53 -0700

    epitaph popup working

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -4,15 +4,6 @@ import tileset from '../../assets/tilemap/TilesetGraveyard.png';
 import mapData from '../../assets/tilemap/Graveyard_Scene.json' with {
 	type: 'json',
 };
-// start of imports for grave interact
-import Blood_Moon_img from '../../assets/font/BloodMoon/BloodMoonPng.json' with {
-	type: 'json',
-};
-import Blood_Moon_xml from '../../assets/font/BloodMoon/BloodMoon.json' with {
-	type: 'json',
-};
-import custom_text from '../util/XMLText.ts';
-// end of imports for grave interact
 
 export default class Play extends Phaser.Scene {
 	private player!: Player;
@@ -23,9 +14,6 @@ export default class Play extends Phaser.Scene {
 	private boneCount: number;
 	private stashCount: number;
 	private boneBuffer: number;
-	// start of variables for grave interact
-    private testButton: Phaser.Input.Keyboard.Key; // button to skip gravestone collision, will be implemented with Zeke's merge
-	// end of variables for grave interact
 
 	constructor() {
 		super({ key: 'playScene' });
@@ -74,136 +62,147 @@ export default class Play extends Phaser.Scene {
 		this.cameras.main.startFollow(this.player, false, 0.1, 0.1);
 		this.cameras.main.fadeIn(1000, 0, 0, 0);
 
-
-		// start of test button listener for grave interact
-        this.testButton = this.input.keyboard?.addKey('E') as Phaser.Input.Keyboard.Key;
-		const stupidThis = this;
-        this.testButton.on("down", function (event) {
-			stupidThis.grave_interact(7);
-        });
-		// end of test button listener for grave interact
-
-		// const { width, height } = this.game.config;
-		// this.bones = this.physics.add.sprite(
-		// 	(width as number / 2) + 50,
-		// 	height as number / 2,
-		// 	'bones',
-		// ).setOrigin(0.5);
-		// this.bones.setScale(0.025);
-		// this.stash = this.physics.add.sprite(
-		// 	width as number / 2,
-		// 	height as number / 2 + 50,
-		// 	'stash',
-		// ).setOrigin(0.5);
-		// this.stash.setScale(0.05);
-		// this.player = new Player(
-		// 	this,
-		// 	width as number / 2,
-		// 	height as number / 2,
-		// 	'player',
-		// 	0,
-		// );
-		// this.boneCountText = this.add.bitmapText(
-		// 	this.player.x,
-		// 	this.player.y - 100,
-		// 	'rocketSquare',
-		// 	this.boneCount.toString(),
-		// 	50,
-		// 	1,
-		// );
-		// //
-		// this.stashCountText = this.add.bitmapText(
-		// 	this.stash.x,
-		// 	this.stash.y - 150,
-		// 	'rocketSquare',
-		// 	this.stashCount.toString(),
-		// 	50,
-		// 	1,
-		// );
-
-		// console.log(this.boneCount.toString(), this.stashCount.toString());
-
-		// this.physics.add.overlap(this.bones, this.player, () => {
-		// 	if (this.boneBuffer > 60 && this.boneCount < 3) {
-		// 		this.boneCount += 1;
-		// 		this.boneBuffer = 0;
-		// 		// this.boneCountText.text = this.boneCount.toString();
-		// 		console.log(this.boneCount);
-		// 	}
-		// });
-
-		// this.physics.add.overlap(this.stash, this.player, () => {
-		// 	this.stashCount += this.boneCount;
-		// 	this.boneCount = 0;
-		// 	// this.boneCountText.text = this.boneCount.toString();
-		// 	// this.stashCountText.text = this.stashCount.toString();
-		// });
-
-		this.physics.add.collider(this.player, wallLayer!);
 	}
 
 	// deno-lint-ignore no-unused-vars
 	override update(time: number, delta: number): void {
 	}
 
-	// handleMovement(){
-	//     this.dog.setVelocityY(0);
-	//     this.dog.setVelocityX(0);
-	//     if(this.up.isDown){
-	//         this.dog.setVelocityY(-400 + (this.boneCount * 50));
-	//     }
-	//     if(this.left.isDown){
-	//         this.dog.setVelocityX(-400 + (this.boneCount * 50));
-	//     }
-	//     if(this.down.isDown){
-	//         this.dog.setVelocityY(400 - (this.boneCount * 50));
-	//     }
-	//     if(this.right.isDown){
-	//         this.dog.setVelocityX(400 - (this.boneCount * 50));
-	//     }
-	//     this.boneCountText.x = this.dog.x;
-	//     this.boneCountText.y = this.dog.y - 100;
-	// }
+}
 
-	grave_interact(grave: number){
-		const window = this.add.graphics();
-    	window.fillStyle(0x000000, 1); // Color and alpha (transparency)
-    	window.fillRect(this.cameras.main.scrollX + this.cameras.main.width/2 - 100, this.cameras.main.scrollY + this.cameras.main.height/2 - 75, 200, 30);
-		window.setScrollFactor(0,0);
+import Phaser from 'phaser';
+import Player from '../prefabs/Player.ts';
+import tileset from '../../assets/tilemap/TilesetGraveyard.png';
+import mapData from '../../assets/tilemap/Graveyard_Scene.json' with {
+	type: 'json',
+};
+
+export default class Play extends Phaser.Scene {
+	private player!: Player;
+	private bones!: Phaser.Physics.Arcade.Sprite;
+	private stash!: Phaser.Physics.Arcade.Sprite;
+	// private boneCountText!: Phaser.GameObjects.BitmapText;
+	// private stashCountText!: Phaser.GameObjects.BitmapText;
+	private boneCount: number;
+	private stashCount: number;
+	private boneBuffer: number;
+	// start of variables for grave interact
+	// end of variables for grave interact
+
+	constructor() {
+		super({ key: 'playScene' });
+		this.boneCount = 0;
+		this.stashCount = 0;
+		this.boneBuffer = 0;
+	}
+
+	init() {
+	}
+
+	preload() {
+		this.load.image('base-tileset', tileset);
+		this.load.tilemapTiledJSON('Graveyard_Scene', mapData);
+		this.load.spritesheet('tilemap_sheet', tileset, {
+			frameWidth: 16,
+			frameHeight: 16,
+		});
+	}
+
+	create() {
+		const map = this.add.tilemap('Graveyard_Scene');
+		const tiles = map.addTilesetImage('TilesetGraveyard', 'base-tileset')!;
+
+		map.createLayer('Ground', tiles, 0, 0);
+		map.createLayer('grass', tiles, 0, 0);
+		map.createLayer('Road', tiles, 0, 0);
+		const wallLayer = map.createLayer('obstacle', tiles, 0, 0);
+		const graveLayer = map.createLayer('Graves', tiles, 0, 0);
+		wallLayer?.setCollisionByProperty({ Collision: true });
+		this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
 		const { width, height } = this.game.config;
-		const graveText = [
-			"Here lies Sir Barksalot, loyal till the end",
-			"Here rests Fido, always chasing tails in the afterlife",
-			"Beloved companion: Rufus, forever fetching in spirit",
-			"In memory of Luna, who howled at the moon one last time",
-			"Here lies Max, who finally found the ultimate chew toy",
-			"RIP Bella, the queen of the backyard",
-			"Here rests Sparky, who followed every squirrel to the great beyond",
-			"In loving memory of Charlie, who ran free at the Rainbow Bridge",
-			"Here lies Daisy, who gave more kisses than any dog could count",
-			"Forever in our hearts: Zeus, the thunderous bark"
-		]
 
-		console.log(graveText[grave]);
+		this.player = new Player(
+			this,
+			width as number / 2,
+			height as number / 2,
+			'player',
+			0,
+		);
 
-		this.add.text(
-			this.cameras.main.scrollX + this.cameras.main.width/2 - 95, 
-			this.cameras.main.scrollY + this.cameras.main.height/2 - 75, 
-			graveText[grave], 
-			{ 
-				fontFamily: 'Arial', 
-				fontSize: 10, 
-				color: '#fff',
-				wordWrap: {
-					width: 190
-				} 
-			}
-		).setScrollFactor(0,0);
+		this.cameras.main.setBounds(0, 0, width as number, height as number);
+		this.cameras.main.setZoom(2);
+		this.cameras.main.setFollowOffset(0);
+		this.cameras.main.startFollow(this.player, false, 0.1, 0.1);
+		this.cameras.main.fadeIn(1000, 0, 0, 0);
+	}
 
-
-		
-
-
+	// deno-lint-ignore no-unused-vars
+	override update(time: number, delta: number): void {
 	}
 }
+
+addToCreate(){
+	const window = this.add.graphics();
+    window.fillStyle(0x000000, 1); // Color and alpha (transparency)
+	window.fillRect(this.cameras.main.width/2 - 100, this.cameras.main.height/2 - 75, 200, 30);
+	window.setScrollFactor(0,0);
+	window.setVisible(false);
+
+	const epitaph = this.add.text(
+		this.cameras.main.width/2 - 95, 
+		this.cameras.main.height/2 - 75, 
+		"", 
+		{ 
+			fontFamily: 'Arial', 
+			fontSize: 10, 
+			color: '#fff',
+			wordWrap: {
+				width: 190
+			} 
+		}
+	).setScrollFactor(0,0);
+	epitaph.setVisible(false);
+
+	const graveText = {
+		"12, 6": "Here lies Sir Barksalot, loyal till the end",
+		"15, 16": "Here rests Fido, always chasing tails in the afterlife",
+		"16, 4": "Beloved companion: Rufus, forever fetching in spirit",
+		"17, 15": "In memory of Luna, who howled at the moon one last time",
+		"18, 7": "Here lies Max, who finally found the ultimate chew toy",
+		"20, 10": "RIP Bella, the queen of the backyard",
+		"21, 5": "Here rests Sparky, who followed every squirrel to the great beyond",
+		"21, 12": "In loving memory of Charlie, who ran free at the Rainbow Bridge",
+		"22, 8": "Here lies Daisy, who gave more kisses than any dog could count",
+		"22, 14": "Forever in our hearts: Zeus, the thunderous bark",
+		"23, 3": "Here sleeps Coco, the fluffiest guardian of dreams",
+		"24, 9": "In memory of Gus, who never met a bone he didn’t like",
+		"25, 5": "Here lies Trixie, who outsmarted the vacuum until the end",
+		"26, 7": "Rest in peace, Benny, who always believed he was a lap dog",
+		"26, 9": "Here lies Nala, who made every car ride an adventure",
+		"18, 15": "In loving memory of Duke, the proud protector of the yard"
+	}
+
+	this.physics.add.collider(this.player, wallLayer!);
+	this.physics.add.collider(this.player, wallLayer!);
+	graveLayer?.setCollisionByProperty({ Interactable: true });
+	const tis = this;
+	this.physics.add.overlap(this.player, graveLayer!, () => {
+		const tile = graveLayer?.getTileAtWorldXY(this.player.x, this.player.y);
+		if (tile?.properties.Interactable === true) {
+			window.setVisible(true);
+			epitaph.setVisible(true);
+			tis.grave_interact(tile, epitaph, graveText);
+		} else {
+			window.setVisible(false);
+			epitaph.setVisible(false);
+		}
+	});
+}
+
+grave_interact(grave: Phaser.Tilemaps.Tile, epitext: Phaser.GameObjects.Text, library: object){	// displays black box with epitaph
+	const key = grave?.x.toString() + ", " + grave?.y.toString();
+	epitext.text = library[key];
+}
+
+
