55a213d9fa45b9c59bdd84979baae0063db27740
= <cjmoshy12@gmail.com>
2024-10-31 18:06:31 -0700

    fmt

--- src/scenes/Credits.ts
+++ src/scenes/Credits.ts
@@ -1,10 +1,3 @@
-import custom_text from '../util/XMLText';
-import Blood_Moon_img from '../../assets/font/BloodMoon/BloodMoonPng.json' with {
-	type: 'json',
-};
-import Blood_Moon_xml from '../../assets/font/BloodMoon/BloodMoon.json' with {
-	type: 'json',
-};
 export default class Credits extends Phaser.Scene {
 	constructor() {
 		super({ key: 'creditScene' });
@@ -14,15 +7,6 @@ export default class Credits extends Phaser.Scene {
 	preload() {}
 	create() {
 		const { width, height } = this.game.config;
-		custom_text(
-			this,
-			'Created',
-			'credits-txt',
-			Blood_Moon_xml.xml,
-			Blood_Moon_img.png,
-			width as number / 2,
-			height as number / 2,
-		);
 
 		this.add.text(width as number / 2, height as number / 2 + 50, 'Return')
 			.setOrigin(0.5).setInteractive().on(
@@ -30,6 +14,7 @@ export default class Credits extends Phaser.Scene {
 				() => this.scene.start('menuScene'),
 			);
 	}
+	// deno-lint-ignore no-unused-vars
 	override update(time: number, delta: number): void {
 	}
 }
